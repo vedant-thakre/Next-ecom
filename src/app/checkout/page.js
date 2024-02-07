dfcb91633104a63ff2208f453d6f3f681c813940
ved-thakre <vedantthakre7@gmail.com>
2024-02-07 17:57:24 +0530

    Checkout page intialized

---
+++ src/app/checkout/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="h-[80vh] w-screen flex items-center justify-center">
+      <p>This is Checkout Page</p>
+    </div>
+  );
+}
+
+export default page
