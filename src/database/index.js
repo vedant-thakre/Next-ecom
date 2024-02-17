c0a58600212bb0c6191211d05f99d6126adecd00
ved-thakre <vedantthakre7@gmail.com>
2024-02-17 18:53:43 +0530

    deployment change

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,11 @@
 import mongoose from "mongoose";
-import colors from "colors";
-
-const configOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
+// added a change
 const connectDB = async () => {
   try {
-    const connectionUrl = process.env.MONGO_URI;
-
-    await mongoose.connect(connectionUrl, configOptions);
-    console.log("Database Connected Successfully");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error(
-      `Error connecting to the database: ${error.message}`.red.bold
-    );
+    console.log(error);
   }
 };
 
