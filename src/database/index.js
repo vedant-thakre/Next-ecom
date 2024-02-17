8613fdee8e16f294a80a2e9433e1affb8ef55ed7
ved-thakre <vedantthakre7@gmail.com>
2024-02-17 19:16:08 +0530

    added mongo uri

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 // added a change
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://vedantthakre7:vedant@cluster0.ig28tkt.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
